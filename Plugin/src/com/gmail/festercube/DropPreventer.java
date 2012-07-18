3d2538715bd3b6d6b55eb634e937eed2d62d8533
Matt Zenzie <festercube@gmail.com>
2012-07-18 18:11:19 -0400

    Maybe

--- Plugin/src/com/gmail/festercube/DropPreventer.java
+++ Plugin/src/com/gmail/festercube/DropPreventer.java
@@ -1,5 +1,18 @@
 package com.gmail.festercube;
 
-public class DropPreventer {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class DropPreventer implements Listener 
+{
+	@EventHandler
+	public void onDropAttempt (PlayerDropItemEvent event)
+	{
+		if(AdminMode.adminModePlayers.contains(event.getPlayer().getPlayerListName()))
+		{
+			event.setCancelled(true);
+		}
+	}
 
 }
